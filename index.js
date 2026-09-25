0a9143d41fdb25bd096a7e0e9bfd4affc5a531a4
prosperoifijen-crypto <prosperoifijen@gmail.com>
2026-09-25 12:36:17 +0100

    Add new commands to COMMANDS object
    Added new commands for weather, time, define, wiki, calc, shorturl, ip, uuid, base64, unbase64, and password.

--- index.js
+++ index.js
@@ -179,6 +179,8 @@ const COMMANDS = {
   GENERAL: [
     "menu","reaper","ping","alive","botinfo","runtime","owner","repo","support","status",
     "profile","help","commands","uptime","version","prefix","jid","chatid","groupinfo","about"
+    "weather","time","define","wiki","calc","shorturl","ip","uuid",
+"base64","unbase64","password",
   ],
   "REAPER SYSTEM": [
     "rank","level","xp","coins","daily","claim","hunt","mission","quest","train","power",
